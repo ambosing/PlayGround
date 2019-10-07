2e140de802101fc6b39e114d9d4582cf67abe055
yangjiwon <ambosing_@naver.com>
2019-10-07 20:40:21 +0900

    삼성 자바 알고리즘 문제 추가

---
+++ java_algorithm/src/SWEA1213.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SWEA1213 {
+	public static void main(String args[])
+	{
+		int test = 0;
+		String str;
+		String input;
+		Scanner sc = new Scanner(System.in);
+		String temp;
+		int cnt = 0;
+		
+		while(test != 10)
+		{
+			test = sc.nextInt();
+			
+			str = new String();
+			input = new String();
+			temp = new String();
+			
+			input = sc.next();
+			
+			str = sc.next();
+			
+			
+			for(int i = 0; i < str.length()-input.length()+1; i++)
+			{
+				temp = str.substring(i, i+input.length());
+				if(input.contentEquals(temp))
+					cnt++;
+			}
+			
+			System.out.println("#"+test+" "+cnt);
+			cnt = 0;
+		}
+	}
+}

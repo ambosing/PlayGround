8cc9b702f0d3621d346fb83135808eec17965f16
Jiwon Yang <ambosing_@naver.com>
2019-10-29 13:02:00 +0900

    SWEA 1220 추가~

--- java_algorithm/src/test.java
+++ java_algorithm/src/test.java
@@ -3,17 +3,15 @@ import java.util.ArrayList;
 public class test {
 	public static void main(String args[])
 	{
-		String[] str = new String[10];
-		int[] arr = new int[100];
-		ArrayList<Integer> arrlist = new ArrayList<Integer>();
+		String str = "123456";
+		String str1 = str;
+		StringBuffer str2 = new StringBuffer();
 		
-		arrlist.add(0, 1);
-		arrlist.add(1, 3);
-		arrlist.add(3, 87);
-		System.out.println(arrlist.get(1)+" "+arrlist.get(3));
-		
-		System.out.println(arr[0]+" "+arr[1]);
-		
-		System.out.println(str.length);
+		str2.append(str);
+		System.out.println(str2);
+		str2.delete(0, 3);
+		System.out.println(str2);
+		str1 = str2.toString();
+		System.out.println(str1);
 	}
 }

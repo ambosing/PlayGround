b7faa2f0b8209af66e9e4c11186befab5bb813a2
ambosing <ambosing_@naver.com>
2021-01-11 10:53:11 +0900

    python code modify

--- Python/Problem Solving/AlgorithmBook/4-4-1 Game Programming.py
+++ Python/Problem Solving/AlgorithmBook/4-4-1 Game Programming.py
@@ -1,74 +1,58 @@
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+def turn_left(di):
+    di -= 1
+    if di < 0:
+        di = 3
+    return di
 
 
-def rotate_left(board, r, c):
-    new_board = [[0] * c for _ in range(r)]
-    for i in range(r):
-        for j in range(c):
-            new_board[j][i] = board[r][c]
-    return new_board
+def is_remain_left(v, r, c):
+    for ii in range(c - 1, -1, -1):
+        if not v[r][ii]:
+            return True
+    return False
 
 
-def is_not_go(board, visit, r, c):
-    for i in range(c - 1, -1, -1):
-        if board[r][i] == 1 or visit[r][i]:
+def is_all_visit(v, x, y):
+    for ii in range(4):
+        nx = dx[ii] + x
+        ny = dy[ii] + y
+        if 0 <= nx <= m and 0 <= ny <= n and not v[ny][nx]:
             return False
     return True
 
 
-def make_visit(board, r, c):
-    visit = [[False] * c for _ in range(r)]
-    for i in range(r):
-        for j in range(c):
-            if board[i][j] == 1:
-                visit[i][j] = True
-    return visit
-
-
-def is_all_visit(visit, r, c):
-    for i in range(4):
-        new_x = dx[i] + c
-        new_y = dy[i] + r
-        if 0 < new_x < n and 0 < new_y < m and visit[new_x][new_y]:
-            return False
-    return True
-
+dx = [0, -1, 0, 1]
+dy = [-1, 0, 1, 0]
 
 n, m = map(int, input().split())
-
 y, x, d = map(int, input().split())
 b = [list(map(int, input().split())) for _ in range(n)]
-v = make_visit(b, n, m)
+v = []
+for i in range(n):
+    lst = []
+    for j in range(m):
+        if b[i][j] == 1:
+            lst.append(True)
+        else:
+            lst.append(False)
+    v.append(lst)
+
 cnt = 0
 while True:
-    if is_not_go(b, v, n, m):
-        v = rotate_left(v, n, m)
-        b = rotate_left(b, n, m)
-        y, x = x, y
-        d = (d + 1) % 4
-        if d == 0 and y > 0 and b[y - 1][x] == 0 and not v[y - 1][x]:
-            y -= 1
-            v[y][x] = True
-        elif d == 1 and x > 0 and b[y][x - 1] == 0 and not v[y][x - 1]:
-            x -= 1
-            v[y][x] = True
-        elif d == 2 and y < n and b[y + 1][x] == 0 and not v[y + 1][x]:
-            y += 1
-            v[y][x] = True
-        elif d == 3 and x < m and b[y][x + 1] == 0 and not v[y][x + 1]:
-            x += 1
-            v[y][x] = True
-        cnt += 1
+    if is_remain_left(v, y, x):
+        d = turn_left(d)
+        for i in range(4):
+            if d != i:
+                continue
+            x = dx[i] + x
+            y = dy[i] + y
     elif is_all_visit(v, x, y):
-        if d == 0 and y < n and b[y + 1][x] == 0:
-            y += 1
-        elif d == 1 and x < m and b[y][x + 1] == 0:
-            x += 1
-        elif d == 2 and y > 0 and b[y - 1][x] == 0:
-            y -= 1
-        elif d == 3 and x > 0 and b[y][x - 1] == 0:
-            x -= 1
+        x = x - dx[d]
+        y = y - dy[d]
+        if b[y][x] == 1:
+            break
     else:
-        v = rotate_left(v, n, m)
-        b = rotate_left(b, n, m)
+        d = turn_left(d)
+    cnt += 1
+print(cnt)
+

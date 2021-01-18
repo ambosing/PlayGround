64651e85c898bce56ae90169fd75be5c2e498d93
ambosing <ambosing_@naver.com>
2021-01-18 23:17:07 +0900

    python code add

--- Python/Problem Solving/AlgorithmBook/7-2-1  Making Tteokbokki Rice Cake.py
+++ Python/Problem Solving/AlgorithmBook/7-2-1  Making Tteokbokki Rice Cake.py
@@ -1,6 +1,3 @@
-from sys import maxsize
-
-
 def rem_height(a):
     num = 0
     for i in h:
@@ -11,12 +8,10 @@ def rem_height(a):
 
 n, m = map(int, input().split())
 h = list(map(int, input().split()))
-h.sort()
+
 lt = 0
-rt = h[-1]
-hap = rt
-pre = maxsize
-res = 0
+rt = max(h)
+
 while lt <= rt:
     mid = (lt + rt) // 2
     height = rem_height(mid)

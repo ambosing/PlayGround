51bba0e903b9a506c3c191fc6d524637d1ca5bc8
ambosing <ambosing_@naver.com>
2021-01-20 20:00:17 +0900

    python code add

--- Python/Problem Solving/Programmers/Triangle_snail.py
+++ Python/Problem Solving/Programmers/Triangle_snail.py
@@ -2,22 +2,17 @@ def solution(n):
     answer = [0] * (n * (n + 1) // 2)
     idx = 0
     cnt = 1
-    m = 0
+    s = 0
     for i in range(n):
-        if i % 3 == 0:
-            for j in range(n - i):
-                idx += m + j
-                answer[idx] = cnt
-                cnt += 1
-            m += 2
-        elif i % 3 == 1:
-            for j in range(n - i):
+        for j in range(n - i):
+            if i % 3 == 0:
+                idx += s + j
+            elif i % 3 == 1:
                 idx += 1
-                answer[idx] = cnt
-                cnt += 1
-        else:
-            for j in range(n - i):
+            else:
                 idx -= n - j - i // 3
-                answer[idx] = cnt
-                cnt += 1
+            answer[idx] = cnt
+            cnt += 1
+        if i % 3 == 0:
+            s += 2
     return answer

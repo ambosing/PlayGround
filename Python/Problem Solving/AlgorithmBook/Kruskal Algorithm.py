d7f2af4b71c8a6a9b711e64fbb3e461e25acbc0b
ambosing <ambosing_@naver.com>
2021-01-28 20:11:18 +0900

    python code add

---
+++ Python/Problem Solving/AlgorithmBook/Kruskal Algorithm.py
@@ -0,0 +1,33 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+edges = []
+result = 0
+v, e = map(int, input().split())
+p = [i for i in range(v + 1)]
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(p, a) != find_parent(p, b):
+        union_parent(p, a, b)
+        result += cost
+
+print(result)

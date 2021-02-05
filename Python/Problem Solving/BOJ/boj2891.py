22144aabb32d9896fe7c68a932cfe0751273af2e
Yang JiWon <ambosing_@naver.com>
2021-02-05 20:28:10 +0900

    python code add

--- Python/Problem Solving/BOJ/boj2891.py
+++ Python/Problem Solving/BOJ/boj2891.py
@@ -1,9 +1,10 @@
 n, s, r = map(int, input().split())
-lst = [0] * (n + 1)
+lst = [0] * (n + 2)
 n_lst = list(map(int, input().split()))
 p_lst = list(map(int, input().split()))
-for i in n_lst:
-    lst[i] = -1
-for i in p_lst:
-    lst[i] = 1
+cnt = s
+while p_lst:
+    a = p_lst.pop()
+    
+print(cnt)
 

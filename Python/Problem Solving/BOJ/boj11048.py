ee7cd0f818b300b73cf9787190eb0f1758d754bb
Yang JiWon <ambosing_@naver.com>
2021-02-06 10:23:26 +0900

    python code add

--- Python/Problem Solving/BOJ/boj11048.py
+++ Python/Problem Solving/BOJ/boj11048.py
@@ -5,9 +5,21 @@ dy = [1, 1, 0]
 
 
 def go(x, y):
-    res =
+    q = deque([(x, y)])
+    d[y][x] = b[y][x]
+    while q:
+        x, y = q.popleft()
+        for i in range(3):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < n and 0 <= nx < m and \
+                    d[ny][nx] < d[y][x] + b[ny][nx]:
+                d[ny][nx] = d[y][x] + b[ny][nx]
+                q.append((nx, ny))
 
 
 n, m = map(int, input().split())
 b = [list(map(int, input().split())) for _ in range(n)]
+d = [[0] * m for _ in range(n)]
 go(0, 0)
+print(d[n - 1][m - 1])

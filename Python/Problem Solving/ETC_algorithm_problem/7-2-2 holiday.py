8f75cc5063bb0d90904ef34ef556556c9c73e35c
ambosing <ambosing_@naver.com>
2021-03-10 20:20:38 +0900

    python code add

---
+++ Python/Problem Solving/ETC_algorithm_problem/7-2-2 holiday.py
@@ -0,0 +1,18 @@
+def dfs(a, b):
+    global res
+    if res < b:
+        res = b
+    if a > n + 1:
+        return
+    for i in range(a, n + 1):
+        dfs(i + lst[i][0], b + lst[i][1])
+
+
+n = int(input())
+lst = [[0, 0]]
+res = 0
+for _ in range(n):
+    t, p = map(int, input().split())
+    lst.append([t, p])
+dfs(1, 0)
+print(res)

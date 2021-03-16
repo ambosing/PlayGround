1eafc364dd1d493e48b1ea7b2a12e1758339d602
Yang JiWon <ambosing_@naver.com>
2021-03-16 22:44:52 +0900

    python code add

--- Python/Problem Solving/ETC_algorithm_problem/7-4-2 change coin.py
+++ Python/Problem Solving/ETC_algorithm_problem/7-4-2 change coin.py
@@ -1,21 +1,24 @@
-def dfs(a):
+def dfs(a, b):
     global res
-    if a > t:
+    if b > t:
         return
-    elif a == t:
+    elif b == t:
         res += 1
     else:
-
+        for i in range(a, n):
+            dfs(i + 1, b + coins[i])
 
 
 t = int(input())
 k = int(input())
 coins = list()
 res = 0
+n = 0
 for _ in range(k):
     money, cnt = map(int, input().split())
+    n += cnt
     for _ in range(cnt):
         coins.append(money)
 coins.sort(reverse=True)
-
-
+dfs(0, 0)
+print(res)

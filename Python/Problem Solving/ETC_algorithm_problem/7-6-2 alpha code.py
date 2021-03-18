44fbba0ef4bac0e63b67099cbd116f051cda1f99
Yang JiWon <ambosing_@naver.com>
2021-03-18 23:08:34 +0900

    python code add

--- Python/Problem Solving/ETC_algorithm_problem/7-6-2 alpha code.py
+++ Python/Problem Solving/ETC_algorithm_problem/7-6-2 alpha code.py
@@ -4,11 +4,24 @@ from string import ascii_uppercase
 def dfs(a):
     global cnt
     if a == len(c):
-        print(''.join(alpha))
+        print(''.join(res))
         cnt += 1
     else:
+        if c[a] == '0':
+            return
+        res.append(alpha[int(c[a]) - 1])
+        dfs(a + 1)
+        res.pop()
+        if a < len(c) - 1 and int(c[a:a+2]) <= 26:
+            idx = int(c[a:a+2]) - 1
+            res.append(alpha[idx])
+            dfs(a + 2)
+            res.pop()
 
 
 c = input()
-alpha = []
+alpha = list(ascii_uppercase)
+res = []
 cnt = 0
+dfs(0)
+print(cnt)

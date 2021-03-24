6b41aca93e0c26ef88fd494c32ef32045b2c5d26
ambosing <ambosing_@naver.com>
2021-03-24 23:26:56 +0900

    python code add

--- Python/Problem Solving/AlgorithmBook/re-4-2 time.py
+++ Python/Problem Solving/AlgorithmBook/re-4-2 time.py
@@ -4,6 +4,6 @@ for i in range(3600 * (n + 1)):
     h = str(i // 3600)
     m = str(i % 3600 // 60)
     s = str(i % 60)
-    if '3' in h or '3' in m or '3' in s:
+    if '3' in h + m + s:
         cnt += 1
 print(cnt)

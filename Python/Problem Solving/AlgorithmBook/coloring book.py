c95e6e455e0fd5fd65b1c8d2724f637845b4c9c4
ambosing <>
2021-05-24 16:13:29 +0900

    a

---
+++ Python/Problem Solving/AlgorithmBook/coloring book.py
@@ -0,0 +1,5 @@
+from collections import deque
+
+n, m = map(int, input().split())
+int
+
---
+++ Python/Problem Solving/test2.py
@@ -0,0 +1,45 @@
+def dfs(g,route):
+
+
+
+def error(g):
+    if error_1(g):
+        return "E1"
+    elif error_2(g):
+        return "E2"
+    return "No Error"
+
+
+def error_1(g):
+    for k, lst in g.items():
+        if len(lst) > 2:
+            return True
+    return False
+
+
+def error_2(g):
+    for k, lst in g.items():
+        s = set(lst)
+        if len(lst) != len(s):
+            return True
+    return False
+
+
+test_case = [('A', 'B'), ('A', 'C'), ('B', 'G'), ('C', 'H'), ('E', 'F'), ('B', 'D'), ('C', 'E')]
+
+graph = {}
+test_case.sort(key=lambda x: (x[0], x[1]))
+root = ""
+for t in test_case:
+    if not root:
+        root = t[0]
+    if t[0] not in graph.keys():
+        graph[t[0]] = [[], [t[1]]]
+    else:
+        graph[t[0]][1].append(t[1])
+    if t[1] not in graph.keys():
+        graph[t[1]] = [[t[0]], []]
+    else:
+        graph[t[1]][0].append(t[0])
+
+
